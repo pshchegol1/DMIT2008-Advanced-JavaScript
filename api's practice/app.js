98ba7545dae3f183c8087828be455a729f3c0e24
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2021-01-19 17:50:27 -0700

    3 buttons Event Listener Added

--- api's practice/app.js
+++ api's practice/app.js
@@ -1,5 +1,6 @@
 window.addEventListener('load',function(e)
 {
+    //Buttons Selectors
     const button1 = document.querySelector('#button1');
     const button2 = document.querySelector('#button2');
     const button3 = document.querySelector('#button3');
@@ -10,19 +11,75 @@ window.addEventListener('load',function(e)
 
 
 
-    
+    //Event Listener
     button1.addEventListener('click', getText);
+    button2.addEventListener('click', getJson);
+    button3.addEventListener('click', getApi);
 
+    //Get Text file data
     function getText()
     {
         fetch('steam.txt')
-        .then(function(res){
+        .then(res =>{
            return res.text();
         })
-        .then(function(data){
+        .then(data => {
             console.log(data);
             output.innerHTML = data;
             output.classList.remove('hidden');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    //Get Json file data
+    function getJson()
+    {
+        fetch('post.json')
+        .then(res =>{
+           return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            let li ='';
+       
+            data.forEach(post => {
+                li += `<li>${post.title}</li>`
+                
+            });
+            output.innerHTML = li;
+           
+            output.classList.remove('hidden');
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    //Get API data
+    function getApi()
+    {
+        fetch('https://api.github.com/users')
+        .then(res =>{
+           return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            let li ='';
+       
+            data.forEach(users => {
+                li += `<li>${users.login}</li>`
+                
+            });
+            output.innerHTML = li;
+           
+            output.classList.remove('hidden');
+
+        })
+        .catch(err => {
+            console.log(err);
         })
     }
 
@@ -35,7 +92,7 @@ window.addEventListener('load',function(e)
 
 
 
-})
+})//End of window Scope
 
 
 
